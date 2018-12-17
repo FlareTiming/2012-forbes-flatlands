05ce4963cd27541b0f909f34758fead891647f26
Phil de Joux <philderbeast@gmail.com>
2018-12-17 15:53:54 -0500

    Add a script to download and pretty print the json locally.

---
+++ download-json.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+curl -X GET -H "Accept:application/json" http://localhost:3000/comps | jq > json/comps.json
+curl -X GET -H "Accept:application/json" http://localhost:3000/nominals | jq > json/nominals.json
+curl -X GET -H "Accept:application/json" http://localhost:3000/pilots | jq > json/pilots.json
+curl -X GET -H "Accept:application/json" http://localhost:3000/tasks | jq > json/tasks.json
+curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/allocation | jq > json/gap-point/allocation.json
+curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/validity | jq > json/gap-point/validity.json
+
+for t in {1..8}
+    do
+        mkdir json/gap-point/$t
+        curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/$t/validity-working | jq > json/gap-point/$t/validity-working.json
+        curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/$t/score | jq > json/gap-point/$t/score.json
+
+        mkdir json/task-length/$t
+        curl -X GET -H "Accept:application/json" http://localhost:3000/task-length/$t/spherical-edge | jq > json/task-length/$t/spherical-edge.json
+    done
